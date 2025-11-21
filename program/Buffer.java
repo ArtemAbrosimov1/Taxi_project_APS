45a88ff8e008c64d9a728c154124fac8f71babd4
ArtemAbrosimov1 <163903171+ArtemAbrosimov1@users.noreply.github.com>
2025-11-21 18:05:20 +0300

    Add files via upload

---
+++ program/Buffer.java
@@ -0,0 +1,91 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Класс буфера для хранения заказов в очереди
+ * Реализует логику управления очередью заказов
+ */
+public class Buffer {
+    private int capacity;
+    private List<Order> orders;
+
+    public Buffer(int capacity) {
+        this.capacity = capacity;
+        this.orders = new ArrayList<>();
+    }
+
+    public boolean isFull() {
+        return orders.size() >= capacity;
+    }
+
+    public boolean isEmpty() {
+        return orders.isEmpty();
+    }
+
+    public void addOrder(Order order) {
+        if (!isFull()) {
+            orders.add(order);
+
+            System.out.println("Заказ" + order.getOrderId() + " добавлен в буфер");
+        }
+    }
+
+    public void removeOrder(Order order) {
+        if (orders.remove(order)) {
+            System.out.println("Заказ " + order.getOrderId() + " удален из буфера");
+        }
+    }
+
+    public Order getLastAddedOrder() {
+        return orders.isEmpty() ? null : orders.get(orders.size() - 1);
+    }
+
+    public void shiftOrders() {
+        if (!orders.isEmpty()) {
+            System.out.println("Сдвиг заказов в буфере");
+
+            if (orders.size() > 1) {
+                orders.remove(0);
+            }
+        }
+    }
+
+
+
+    public Order selectOrderByPriority() {
+        if (orders.isEmpty()) {
+            return null;
+        }
+
+        Order selected = orders.get(0);
+        for (Order order : orders) {
+            if (order.getPriority().getNumericalValue() > selected.getPriority().getNumericalValue()) {
+                selected = order;
+            }
+            else if (order.getPriority() == selected.getPriority()) {
+                if (order.getArrivalTime().isAfter(selected.getArrivalTime())) {
+                    selected = order;
+                }
+            }
+        }
+
+        return selected;
+    }
+
+    public List<Order> getOrders() {
+        return new ArrayList<>(orders);
+    }
+
+     public int getSize() {
+        return orders.size();
+     }
+
+     public int getCapacity() {
+        return capacity;
+     }
+}
+
+
